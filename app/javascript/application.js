d7b3117fecabf6802ec02d9d8feb339d276f8fff
Luis Fernando Flores <fernando.flores1961@gmail.com>
2022-10-23 00:11:09 -0500

    Add Bootstrap vertical form styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// This code will be replaced to use the button to get to the adds index
+// let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
+// let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
+//   return new bootstrap.Popover(popoverTriggerEl)  
+// })
