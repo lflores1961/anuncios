e6daa1f34d214bb789a722cba53834438922ab33
Fernando Flores <fernando.flores1961@gmail.com>
2022-11-13 21:43:24 -0600

    Add turbo tags and turbo frames for broadcasting preparation

--- app/models/add.rb
+++ app/models/add.rb
@@ -2,6 +2,10 @@ class Add < ApplicationRecord
   scope :descending, -> { order("created_at DESC") }
   scope :ascending, -> { order("created_at ASC") }
 
+  after_create_commit { broadcast_prepend_to "adds" }
+  after_update_commit { broadcast_replace_to "adds" }
+  after_destroy_commit { broadcast_remove_to "adds" }
+
   validates_presence_of :title, :description
   validates :title, length: { minimum: 3, maximum: 100 }
   validates :description, length: { minimum: 3, maximum: 2000 }
